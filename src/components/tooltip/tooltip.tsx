14d564341d4938aac0e15428d950d4acf174411c
Mohamed Sayed <mohamed.sayed@tradeling.com>
2022-01-16 00:33:14 +0400

    refactor: test tooltip on the screen

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -11,8 +11,9 @@ export function Tooltip(props: TooltipProps) {
   const { children, className = '', arrowProps = {}, open, ...rest } = props;
   const { className: arrowClassName, ...arrowRest } = arrowProps;
   if (!open) return null;
+  //todo: add tooltip placement e.g (top, bottom, start, end etc...) default is bottom
   return (
-    <div className={`bg-grayLight mt-md p-lg relative rounded-sm ${className}`} {...rest}>
+    <div className={`bg-grayLight p-lg absolute rounded-sm ${className}`} {...rest}>
       <div
         className={`w-0 h-0 border-8 border-transparent border-b-grayLight absolute -top-4 left-8 ${arrowClassName}`}
         {...arrowRest}
--- src/screens/home/screen.tsx
+++ src/screens/home/screen.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { BaseButton } from '@components/base-button/base-button';
 import { Input } from '@components/input/input';
+import { ScreenContainer } from '@components/screen-container/screen-container';
+import { Tooltip } from '@components/tooltip/tooltip';
+import debounce from 'lodash/debounce';
+import { Heading } from '@components/heading/heading';
 
 export function HomeScreen() {
+  const [inputValue, setInputValue] = useState('');
+  //todo: move into container
+  const handleInputChange = (event: any) => {
+    setInputValue(event.target.value);
+  };
+  const debouncedInputChange = useCallback(debounce(handleInputChange, 300), []);
   return (
-    <div className='flex pt-xl'>
-      <Input className='ml-lg' />
-      <BaseButton className='bg-primary ml-lg'>Home Screen</BaseButton>
+    <div className='w-full'>
+      <Heading as='h1' className='p-lg max-w-max'>
+        Deezer
+      </Heading>
+      <ScreenContainer className='flex flex-col p-xl justify-center items-center'>
+        <div className='flex justify-center'>
+          <div className='w-96 relative'>
+            <Input className='w-full' defaultValue={inputValue} onChange={debouncedInputChange} />
+            <Tooltip open={!!inputValue} className='mt-md w-full'>
+              Content
+            </Tooltip>
+          </div>
+
+          <BaseButton className='bg-primary ml-lg'>Home Screen</BaseButton>
+        </div>
+      </ScreenContainer>
     </div>
   );
 }
