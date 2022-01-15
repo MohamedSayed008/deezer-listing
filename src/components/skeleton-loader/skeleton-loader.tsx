ce042a38d2e6eb1629034b24cc72a4cc8c04e8f6
Mohamed Sayed <mohamed.sayed@tradeling.com>
2022-01-15 23:05:50 +0400

    feat: add skeleton loader

---
+++ src/components/skeleton-loader/skeleton-loader.tsx
@@ -0,0 +1,22 @@
+import React, { DetailedHTMLProps, HTMLAttributes } from 'react';
+import skeleton from '@styles/skeleton.module.css';
+
+interface SkeletonLoaderProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  type?: 'pulse' | 'wave';
+  isText?: boolean;
+  height?: string;
+  width?: string;
+}
+
+export const SkeletonLoader = (props: SkeletonLoaderProps) => {
+  const { type = 'pulse', isText = false, className = '', height = 'h-6', width = 'w-full', ...rest } = props;
+
+  return (
+    <div
+      className={`${skeleton.loader} ${isText ? `${skeleton.text}` : ''} ${
+        skeleton[type]
+      } ${height} ${width} ${className}`}
+      {...rest}
+    />
+  );
+};
