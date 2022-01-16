b5693e048c1ec74eb3117aaaf4f74c1def2238c3
Mohamed Sayed <mohamed.sayed@tradeling.com>
2022-01-16 22:16:40 +0400

    feat: add album and track types

--- src/screens/home/types.ts
+++ src/screens/home/types.ts
@@ -13,3 +13,48 @@ export interface Artist {
   radio: boolean;
   tracklist: string;
 }
+
+export interface Album {
+  id: number;
+  title: string;
+  link: string;
+  cover: string;
+  cover_small: string;
+  cover_medium: string;
+  cover_big: string;
+  cover_xl: string;
+  md5_image: string;
+  genre_id: number;
+  fans: number;
+  release_date: Date;
+  record_type: string;
+  tracklist: string;
+  explicit_lyrics: boolean;
+  type: string;
+}
+
+export interface Track {
+  id: string;
+  readable: true;
+  title: string;
+  title_short: string;
+  title_version: string;
+  isrc: string;
+  link: string;
+  duration: number;
+  track_position: number;
+  disk_number: number;
+  rank: string;
+  explicit_lyrics: boolean;
+  explicit_content_lyrics: number;
+  explicit_content_cover: number;
+  preview: string;
+  md5_image: string;
+  artist: {
+    id: string;
+    name: string;
+    tracklist: string;
+    type: string;
+  };
+  type: string;
+}
